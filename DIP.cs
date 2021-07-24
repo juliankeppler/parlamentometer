8ea6cb57f7568384615a37ffb2acda291fc2471d
Julian Keppler <julian@juliankeppler.de>
2021-07-24 13:34:28 +0200

    Implementierung Document Requests

--- DIP.cs
+++ DIP.cs
@@ -3,18 +3,27 @@ using System.Net;
 using System.Web;
 using System.Linq;
 using System.Collections.Specialized;
+using System.Collections.Generic;
 using Newtonsoft.Json;
+using System.Threading;
 
 
 public class DIP {
-    private static string APIKey = "SbGXhWA.3cpnNdb8rkht7iWpvSgTP8XIG88LoCrGd4";
-    private static string URLBase = "https://search.dip.bundestag.de/search-api/v1/advanced/search";
-    private WebClient wc = new WebClient();
+    private static string APIKey = "SbGXhWA.3cpnNdb8rkht7iWpvSgTP8XIG88LoCrGd4"; //The public API-Key for the DIP-API
+    private static string URLBase = "https://search.dip.bundestag.de/search-api/v1/advanced/search"; //The base URL for all requests to the DIP-API
+    private WebClient wc = new WebClient(); // The client used to perform all http requests to the DIP APi
 
-    private string ToQueryString(NameValueCollection nvc) {
+    private Dictionary<string, int> numFoundCache = new Dictionary<string, int>{};
+
+    /// <summary>
+    /// Converts a given NameValueCollection with query arguments into a query string.
+    /// </summary>
+    /// <param name="args">The <see cref="System.Collections.Specialized.NameValueCollection"/> holding the query arguments as key, value pairs.</param>
+    /// <returns>A query <see cref="System.String"/>.</returns>
+    private string ToQueryString(NameValueCollection args) {
         var array = (
-            from key in nvc.AllKeys
-            from value in nvc.GetValues(key)
+            from key in args.AllKeys
+            from value in args.GetValues(key)
                 select string.Format(
                     "{0}={1}",
                     HttpUtility.UrlEncode(key),
@@ -23,42 +32,126 @@ public class DIP {
         return "?" + string.Join("&", array);
     }
 
+    /// <summary>
+    /// Performs an http request to the DIP API.
+    /// </summary>
+    /// <param name="args">A <see cref="System.Collections.Specialized.NameValueCollection"/> holding the query arguments for the request.</param>
+    /// <returns>A <see cref="System.Dynamic"/> object representing the response of the API.</returns>
+    /// <exception cref="System.ArgumentException"><paramref name="args"/> holds invalid parameters.</exception>
     private dynamic Request(NameValueCollection args) {
+
+        // Filter for invalid arguments
         if (args["term"] == "") {
             throw new ArgumentException("No search term provided!");
         }
 
+        // Build request URL from args
         string requestURL = URLBase+ToQueryString(args);
 
+        // Do request
         string json = wc.DownloadString(requestURL);
+
+        // Convert json into dynamic object
         dynamic obj = JsonConvert.DeserializeObject<dynamic>(json);
 
         return obj;
     }
+
+    private NameValueCollection SetDefaultParams(string term, int[] electionPeriod) {
+        NameValueCollection args = new NameValueCollection();
+        args.Add("term", term);
+        args.Add("sort", "datum_auf");
+        args.Add("f.aktivitaetsart_p", "05Reden, Wortmeldungen im Plenum");
+        foreach (int i in electionPeriod) {
+            args.Add("f.wahlperiode", i.ToString());
+        }
+        return args;
+    }
+
     public DIP() {
+        // Set authorization headers to access the advanced DIP API
         wc.Headers.Add("Authorization", "ApiKey "+DIP.APIKey);
         wc.Headers.Add("Origin", "https://dip.bundestag.de");
         wc.Headers.Add("Referer", "https://dip.bundestag.de/");
     }
 
     public int GetResults(string term, int[] electionPeriod) {
-        NameValueCollection args = new NameValueCollection();
-        args.Add("term", term);
-        args.Add("sort", "datum_auf");
-        foreach (int i in electionPeriod) {
-            args.Add("f.wahlperiode", i.ToString());
-        }
-        args.Add("rows", "1");
+
+        // Create request query
+        NameValueCollection args = SetDefaultParams(term, electionPeriod);
+        args.Add("rows", "0"); // We don't need to request any document data to get the number of results
+        
+
+        // Do the request
         dynamic resp;
         try {
             resp = Request(args);
         } catch (ArgumentException) {
             throw;
         }
-        return resp.numFound;
+
+        // Add number found to cache and return
+        int n = resp.numFound;
+        numFoundCache.Add(term+electionPeriod.ToString(), n);
+        return n;
     }
     
     public int GetResults(string term) {
-        return GetResults(term, new int[] {});
+        return GetResults(term, new int[]{});
     }
+
+    private string[] GetDocuments(string term, int[] electionPeriod) {
+        NameValueCollection args = SetDefaultParams(term, electionPeriod);
+
+        int n = 0;
+        if (numFoundCache.ContainsKey(term+electionPeriod.ToString())) {
+            n = numFoundCache[term+electionPeriod.ToString()];
+        } else {
+            n = GetResults(term, electionPeriod);
+        }
+        
+        int batchsize = 500;
+        args.Add("rows", batchsize.ToString());
+        int requests = (int)Math.Ceiling((decimal)n/batchsize);
+
+        if (requests>20) {
+            throw new InvalidOperationException("Too many results for given search term.");
+        }
+
+        List<string> res = new List<string>{};
+
+        for (int i = 0; i < requests; i++)
+        {
+            args.Set("start", (i*batchsize).ToString());
+            Console.WriteLine($"n={n}, batchsize={batchsize}, i={i}, start={i*batchsize}, progress={String.Format("{0:P0}", (double)res.Count/n)}");
+
+            if (i%4==0 && i != 0) {
+                Console.WriteLine("Sleeping");
+                Thread.Sleep(1000);
+            }
+
+            dynamic resp;
+            try {
+                resp = Request(args);
+            } catch (ArgumentException) {
+                throw;
+            }
+            
+            foreach (dynamic document in resp.documents) {
+                res.Add(document.datum.ToString());
+            }
+        }
+        Console.WriteLine($"{res.Count}");
+        return null;
+    }
+    
+    public Dictionary<string, int> GetRelevance(string term, int[] electionPeriod) {
+        GetDocuments(term, electionPeriod);
+        return null;
+    }
+
+    public Dictionary<string, int> GetRelevance(string term) {
+        return GetRelevance(term, new int[]{});
+    }
+
 }
--- Program.cs
+++ Program.cs
@@ -4,15 +4,31 @@ using ScottPlot;
 namespace sweproject {
     class Program {
         static void Main(string[] args) {
-            DIP d = new DIP();
+            Console.WriteLine("Start");
+            DateTime starttime = DateTime.Now;
+
+            //////
+            string term = "corona";
+            DIP dip = new DIP();
             int results;
             try {
-                results = d.GetResults("Corona", new int[] {18,19});
+                results = dip.GetResults(term);
                 Console.WriteLine(results);
             } catch (ArgumentException) {
                 // Ask user to provide a search term
                 Console.WriteLine("Invalid search term");
             }
+            try {
+                dip.GetRelevance(term);
+            } catch (ArgumentException) {
+                // Ask user to provide a search term
+                Console.WriteLine("Invalid search term");
+            }
+            /////
+            
+            DateTime endtime = DateTime.Now;
+            TimeSpan runtime = endtime - starttime;
+            Console.WriteLine(runtime.ToString());
         }
     }
 }
